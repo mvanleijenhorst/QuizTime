a5d69561faa704d51386c4d1c3c9974a2efe5fb2
mark <mark.vanleijenhorst@foribus.nl>
2023-05-17 11:45:51 +0200

    Add singleton

---
+++ QuizTime.Wpf/QuizSession.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace QuizTime.Wpf;
+
+public class QuizSession
+{
+    public static QuizSession GetInstance()
+    {
+        return _instance;
+    }
+
+    private static readonly QuizSession _instance = new();
+
+    private QuizSession()
+    {
+        Data = new Dictionary<string, object>();
+
+    }
+
+    public Dictionary<string, object> Data { get; }
+
+}
